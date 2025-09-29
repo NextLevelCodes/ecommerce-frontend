c831931bb312fd49f4d6ff5a066a18545fd14120
Satis9815 <satiskumarchaudhary52@gmail.com>
2025-09-29 11:18:24 +0545

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   const name = 'satis';
   const num = 10;
-  return <h1>Hello world</h1>;
+  return (
+    <h1>
+      Hello world
+      <img src="" alt="" />
+    </h1>
+  );
 }
